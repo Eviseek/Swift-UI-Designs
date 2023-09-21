9bd7daee7604563ab2c3ac2e3b6dcc4279ff81ad
Eva Chlpikova <chlpikova.e@gmail.com>
2023-09-21 18:26:13 +0200

    Initial Commit

---
+++ Swift-UI-Designs/Swift_UI_DesignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_DesignsApp.swift
+//  Swift-UI-Designs
+//
+//  Created by Eva Chlpikova on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_DesignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
